c475a17f9e1bec1ae3763a415191ff17ae6e545f
sato_naoki <sato.naoki@alhinc.jp>
2025-11-17 14:43:14 +0900

    first commit

---
+++ src/main/java/com/example/mugichaLatte/MugichaLatteApplication.java
@@ -0,0 +1,13 @@
+package com.example.mugichaLatte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MugichaLatteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MugichaLatteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mugichaLatte/MugichaLatteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mugichaLatte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MugichaLatteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
