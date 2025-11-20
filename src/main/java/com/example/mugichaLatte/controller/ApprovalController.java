156d13f836bf97582b328d7686808cc193f5f7d1
hayashi_temma <hayashi.temma@alhinc.jp>
2025-11-20 13:01:16 +0900

    承認・差戻機能実装//林

---
+++ src/main/java/com/example/mugichaLatte/controller/ApprovalController.java
@@ -0,0 +1,49 @@
+package com.example.mugichaLatte.controller;
+
+import com.example.mugichaLatte.repository.entity.Attendances;
+import com.example.mugichaLatte.service.ApprovalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class ApprovalController {
+    @Autowired
+    ApprovalService approvalService;
+
+    @GetMapping("/approve")
+    public ModelAndView showApprovalList(){
+        ModelAndView mav = new ModelAndView("approve");
+        List<Attendances> list = approvalService.getApprovalList();
+        mav.addObject("attendances", list);
+        return mav;
+    }
+
+    @PostMapping("/approve")
+    public String permitStatus(@RequestParam(value="attendanceId", required = false) List<Integer>ids,
+                                     @RequestParam("mode") String mode,
+                                     RedirectAttributes redirectAttributes){
+
+        List<String> errorMessages = new ArrayList<String>();
+        if (ids == null) {
+            // エラーを一度だけ表示するためにFlash属性を使う
+            errorMessages.add("1つ以上選択してください");
+            redirectAttributes.addFlashAttribute("errorMessages", errorMessages);
+            return "redirect:/approve";
+        }
+
+        if(mode.equals("permit")) {
+            approvalService.permit(ids);
+        } else if (mode.equals("reject")) {
+            approvalService.reject(ids);
+        }
+        return "redirect:/approve";
+    }
+}
--- src/main/java/com/example/mugichaLatte/repository/AttendancesRepository.java
+++ src/main/java/com/example/mugichaLatte/repository/AttendancesRepository.java
@@ -2,6 +2,7 @@ package com.example.mugichaLatte.repository;
 
 import com.example.mugichaLatte.repository.entity.Attendances;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -28,4 +29,14 @@ public interface AttendancesRepository extends JpaRepository<Attendances, Intege
     boolean existsByUserIdAndDate(int userId, LocalDate date);
     // 申請済かつ未承認の勤怠一覧
     List<Attendances> findByStatusAndApprovalStatus(int status, int approvalStatus);
+
+    //承認用のUPDATE
+    @Modifying
+    @Query("UPDATE Attendances a SET a.approvalStatus = 1 WHERE a.id = :id")
+    void approve(@Param("id") Integer id);
+
+
+    @Modifying
+    @Query("UPDATE Attendances a SET a.approvalStatus = 2, a.status = 0 WHERE a.id = :id")
+    void reject(@Param("id") Integer id);
 }
